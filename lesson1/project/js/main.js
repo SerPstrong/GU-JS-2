3c35bf9683c6bb942285a920a4268a2eef8c1bd5
serpstrong <ommuts@gmail.com>
2019-10-27 22:19:41 +0500

    2 tasks

--- lesson1/project/js/main.js
+++ lesson1/project/js/main.js
@@ -1,20 +1,24 @@
 const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500},
+    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 4, title: 'Gamepad', price: 4500},
 ];
 
 const renderProduct = (title, price) => {
-  return `<div class="product-item">
+    return `<div class="product-item">
             <h3>${title}</h3>
             <p>${price}</p>
           </div>`;
 };
 
+// const renderProducts = goodsList => {
+//   const productsList = goodsList.map(good => renderProduct(good.title, good.price));
+//   document.querySelector('.products').innerHTML = productsList;
+// };
+
 const renderProducts = goodsList => {
-  const productsList = goodsList.map(good => renderProduct(good.title, good.price));
-  document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = goodsList.map(good => renderProduct(good.title, good.price));
 };
 
 renderProducts(products);
